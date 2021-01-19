d99030494c619cca5e5c9004deee2cb09154f060
satyam52 <satyam0350@gmail.com>
2021-01-19 09:14:43 +0530

    added all files

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import "./App.css";
+import Editor from "./component/editor";
+
+function App() {
+  return (
+    <div className="App">
+      <Editor />
+      {/* <Parser /> */}
+    </div>
+  );
+}
+
+export default App;
